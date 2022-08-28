a2241e92c0b3f8dc7ad968d6757bf40a7016deea
Tanvie <tanviee09@gmail.com>
2022-08-28 21:28:17 +0530

    before rpi

---
+++ esp-rpi/esp-rpi.ino
@@ -0,0 +1,78 @@
+#include <ArduinoJson.h>
+#include <ESP32Servo.h>
+
+DynamicJsonDocument doc(1024);
+
+Servo p;
+Servo t;
+
+
+int width = 640, height = 480; // Camera resoulution
+int xPos = 90, yPos = 50;      // initial positions of Servos
+const int angle = 3;
+
+int loco = 0;
+int sos = 0;
+int pan = 0;
+int tilt = 0;
+String temp = "";         // commands
+
+void setup() {
+  p.attach(12);
+  t.attach(14);
+  p.write(xPos);
+  t.write(yPos);
+
+  Serial.begin(38400);
+
+}
+
+
+void loop() {
+
+
+
+  if (Serial.available() > 0)
+  {
+    temp = Serial.readStringUntil('*');
+    Serial.println(temp);
+    DeserializationError err = deserializeJson(doc,  temp);
+    if (err) {
+      Serial.println(" Proximity deserializeJson() failed: ");
+    }
+
+
+    loco = doc["loco"];
+    sos = doc["sos_status"];
+    pan = doc["pan"];
+    tilt = doc["tilt"];
+
+    Serial.println(pan);
+
+
+    if (pan > width / 2 + 100)
+      xPos -= angle;
+    if (pan < width / 2 - 100)
+      xPos += angle;
+    if (tilt < height / 2 + 100)
+      yPos -= angle;
+    if (tilt > height / 2 - 100)
+      yPos += angle;
+
+
+
+    //    if (yPos >= 180)
+    //      yPos = 180;
+    //    else if (yPos <= 0)
+    //      yPos = 0;
+
+    p.write(xPos);
+    t.write(yPos);
+
+
+
+    temp = "";
+    Serial.println("");
+    delay(15);
+  }
+}
